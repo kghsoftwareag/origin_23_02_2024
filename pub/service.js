8409d5152945f6698c732de0507a662905613ea0
kgh <kgh@softwareag.com>
2024-02-23 18:16:03 +0530

    seven

---
+++ pub/service.js
@@ -0,0 +1,17 @@
+app.service('myserv', function() {
+          this.getServiceList = function () {
+    return [];
+}
+this.getISEndpoint = function() { 
+ return 'http://SAG-9BTJYD3:5525/';
+}
+this.getAPIList = function() { 
+ return [];
+}
+this.getCreatedTime = function() { 
+ return "23-02-2024 16:34:16 IST";
+}
+this.getPackageInfo = function(){
+ return{"packageName":"origin_23_02_2024","createdDate":"23-02-2024 16:22:43 IST","version":"1.0"};
+}
+});
